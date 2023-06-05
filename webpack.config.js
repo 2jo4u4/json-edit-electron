ec935577efd26ba9276fe9035836ead5ba17e34b
Jay Huang 黃冠傑 (奧圖碼) <jay.huang@optomade.com>
2023-06-05 14:09:52 +0800

    add local dev with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/source.js",
@@ -19,4 +20,10 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
 };
